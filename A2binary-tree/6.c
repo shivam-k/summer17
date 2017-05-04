7fd96853b5e578afd85441e8e8fbf75c00ff1962
shivam-k <shivamkumar30013@gmail.com>
2017-05-04 22:41:15 +0530

    Thu May  4 22:41:14 IST 2017

--- A2binary-tree/6.c
+++ A2binary-tree/6.c
@@ -10,19 +10,57 @@ int main()
 	int max=arr[0], i=1;
 	int n=sizeof(arr)/sizeof(arr[0]);
 
-	int col[]={2, 5, 6, 8, 3, 4, 1, 7};
-	int size=sizeof(col)/sizeof(col[0]);
-	int l, r, j=2;
+	// int col[]={2, 5, 6, 8, 3, 4, 1, 7};
+	//int size=sizeof(col)/sizeof(col[0]);
+	int l, r, j=2, pass=0, t1, t2;
+	// while(1)
+	// {
+	// 	printf("%d\n", j);
+		
+	// 	if(pass==0)
+	// 	{
+	// 		j=left[j];
+	// 		pass=1;
+	// 	}
+	// 	else if(pass==1)
+	// 	{
+	// 		j=right[j];
+	// 		pass=0;
+	// 	}
+	// 	if(j==-1)
+	// 		break;
 
+	// }
+
+
+	printf("%d ", arr[2]);
 	while(1)
 	{
-		
 		l=left[j];
 		r=right[j];
 		if(l!=-1)
 			printf("%d ", arr[l]);
 		if(r!=-1)
 			printf("%d ", arr[r]);
+		
+		if(pass==0)
+		{
+			printf("l-%d\n", j);
+			t1=j;
+			j=left[t1];
+			t1=j;
+			pass=1;
+		}
+		else if(pass==1)
+		{
+			printf("r-%d\n", j);
+			t2=j;
+			j=right[t2];
+			t2=j;
+			pass=0;
+		}
+		if(t1==-1 && t2==-1)
+			break;
 	}
 	
 	printf("\n");
